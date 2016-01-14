d4c4662317322bbd03e77f57a034217ddc262aee
bringmehome <bringmehome@vip.qq.com>
2016-01-14 11:09:30 +0800

    add server.js

--- server.js
+++ server.js
@@ -24,3 +24,17 @@ app.listen(port, function(error) {
         console.info("Open up http://localhost:%s/ in your browser.", port, port)
     }
 })
+
+
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var config = require('./webpack.config');
+
+// new WebpackDevServer(webpack(config), {
+//     publicPath: config.output.publicPath,
+//     hot: true,
+//     historyApiFallback: true
+// }).listen(3000, '192.168.7.254', function(err, result) {
+//     if (err) console.log(err);
+//     console.log('Listening at 192.168.7.254:3000');
+// });
